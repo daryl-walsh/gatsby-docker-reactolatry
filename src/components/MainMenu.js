251d16ed5cd5a929a8c4ed3cd65b448a8908011f
Daryl <darylwalsh@gmail.com>
2019-04-22 05:56:47 -0400

    feat: add basic styled components

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,6 +1,24 @@
 import React from 'react'
 import { graphql, StaticQuery, Link } from 'gatsby'
+import styled from 'styled-components'
+import SiteInfo from './SiteInfo'
+const MainMenuWrapper = styled.div`
+  display: flex;
+  background-color: rgb(3, 27, 77);
+`
 
+const MainMenuInner = styled.div`
+  max-width: 960px;
+  margin: 0 auto;
+  display: flex;
+  width: 960px;
+  height: 100%;
+`
+const MenuItem = styled(Link)`
+  color: white;
+  display: block;
+  padding: 8px 16px;
+`
 const MainMenu = () => (
   <StaticQuery
     query={graphql`
@@ -21,15 +39,18 @@ const MainMenu = () => (
       }
     `}
     render={props => (
-      <div>
-        {props.allWordpressWpApiMenusMenusItems.edges[0].node.items.map(
-          item => (
-            <Link to={item.object_slug} key={item.title}>
-              {item.title}
-            </Link>
-          ),
-        )}
-      </div>
+      <MainMenuWrapper>
+        <MainMenuInner>
+          <SiteInfo />
+          {props.allWordpressWpApiMenusMenusItems.edges[0].node.items.map(
+            item => (
+              <MenuItem to={item.object_slug} key={item.title}>
+                {item.title}
+              </MenuItem>
+            ),
+          )}
+        </MainMenuInner>
+      </MainMenuWrapper>
     )}
   />
 )
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import MainMenu from './MainMenu'
 import Header from './header'
-import './layout.css'
+import styled, { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto+Slab');
+  @import url('https://cdn.jsdelivr.net/npm/hack-font@3/build/web/hack-subset.css');
+  @import url('https://fonts.googleapis.com/css?family=Monofett');
+  body{
+    font-family: 'Roboto Slab', serif;
+    margin: 0 !important;
+    padding: 0 !important;
+  }
+
+  header.text{
+    font-family: 'Monofett', cursive;
+  }
+`
+const LayoutWrapper = styled.div`
+  max-width: 960px;
+  margin: 0 auto;
+`
 
 const Layout = ({ children }) => (
   <div>
+    <GlobalStyles />
     <MainMenu />
-    {children}
+    <LayoutWrapper>{children}</LayoutWrapper>
   </div>
 )
 
